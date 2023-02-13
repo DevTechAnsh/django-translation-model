6fb27458fe475d7db25f0c128281df0519911617
Ansh <anshdevkumar50@gmail.com>
2023-02-13 15:21:41 +0530

    Added book model and migrations file

--- book_shop/book/models.py
+++ book_shop/book/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    book_name = models.JSONField(default=dict)
+    author_name = models.CharField(max_length=255, null=True, blank=True)
+    published_date = models.DateField(auto_now=True)
