d04788f900627c96daf2ab199b7adfd3263b9bcf
Ansh <anshdevkumar50@gmail.com>
2023-02-13 15:22:50 +0530

    added the BookTranslationForm for book model and added in django admin

--- book_shop/book/admin.py
+++ book_shop/book/admin.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.contrib import admin
+from django import forms
+from .forms import BookTranslationForm
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        return BookTranslationForm
+
+    def save_model(self, request, obj, form, change):
+        form.cleaned_data.pop('author_name')
+        obj.book_name = {key.replace('book_name_', ''): value for key, value in form.cleaned_data.items()}
+        obj.save()
+
+admin.site.register(Book, BookAdmin)
---
+++ book_shop/book/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.conf import settings
+
+from .models import Book
+
+class BookTranslationForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = '__all__'
+        exclude = ["book_name"]
+
+    def __init__(self, *args, **kwargs):
+        for key,val in settings.LANGUAGES:
+            self.base_fields[f'book_name_{key}'] = forms.CharField(required=True, label=f'Book Name ({key})')
+            super().__init__ (*args, **kwargs)
+            if self.instance:
+                self.initial.update({f"book_name_{k}": v for k, v in self.instance.book_name.items ()})
